32a2e5af6ec5bc182fff57bb2275938a6470517a
Zhang <zhangire27@7hills.org>
2026-09-22 10:29:09 -0400

    yo

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
+		System.out.println("Vibu is my goat");
+		int test = 2;
+		int test2 = 4;
+		System.out.println(test + test2);
 	}
 }
