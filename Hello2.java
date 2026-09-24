cf0ce2c2ce9522da8cfb78389278f4cc8d0c69b8
Zhang <zhangire27@7hills.org>
2026-09-24 10:54:54 -0400

    I made the setup

--- Hello2.java
+++ Hello2.java
@@ -1,5 +1,5 @@
 public class Hello2 {
-	public static voic main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Yo");
 	}
 }
--- LemonadeStand.java
+++ LemonadeStand.java
@@ -6,57 +6,76 @@ import java.util.Scanner;
 
 public class LemonadeStand {
         
+        int daysToSell = 7;
 
-        double cashOnHand;
-
+        double cashOnHand = 20.0;
         double costofLemon = 1.50; // 1 lemon
-
         double costofSugar = 2.00; // 1 cup
-
         double costofIce = 0.75; // 1 cup
-
         double costofCups = 0.50; // 1 cup
 
-        
-
         int qtyLemons = 0;
-
         int qtySugars = 0;
-
         int qtyIce = 0;
-
         int qtyCups = 0;
-
         
-
         int lemonsPerPitcher; // initialize now, ask for user input later
-
         int sugarsPerPitcher;
-
         int icePerPitcher;
-
-        int cupsPerPitcher;
-
+        int cupsPerPitcher = 10;
         
-
         boolean moreCupsInPitcher = true;
-
         int totalCustomers = 0;
-
         int cupsRemaining;
 
-        
-
         double temperature; // randomized; could be on a scale from 1-10 where 1 is lowest temp $
-
                                                                  
-        public void setup() {
+        public boolean setup() {
 		
-		Scanner scan = new Scanner(System.in);
-		int lemons = scan.nextInt(); // still need to clear buffer
-		String temp = scan.nextLine(); // this line doesn't allow for user input bc enter is hanging
-	
-	}                               
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.println("....................");
+			System.out.println("1 lemon: $" + costofLemon);
+			System.out.println("1 cup of sugar: $" + costofSugar);
+			System.out.println("1 cup of ice: $" + costofIce);
+			System.out.println("1 cup: $" + costofCups);
+			System.out.println("....................");
+			System.out.println("\n");
+			
+			System.out.println("You have $" + cashOnHand + ". How many lemons would you like to purchase?");
+			qtyLemons = scan.nextInt(); // still need to clear buffer
+			//String temp = scan.nextLine(); // this line doesn't allow for user input bc enter is hanging -> handling storage issue as a buffer
+			cashOnHand = cashOnHand - (qtyLemons * costofLemon);
+			
+			System.out.println("You have $" + cashOnHand + ". How many cups of sugar would you like to purchase?");
+			qtySugars = scan.nextInt();
+			cashOnHand = cashOnHand - (qtySugars * costofSugar);
+			
+			System.out.println("You have $" + cashOnHand + ". How many cups of ice would you like to purchase?");
+			qtyIce = scan.nextInt();
+			cashOnHand = cashOnHand - (qtyIce * costofIce);
+			
+			System.out.println("You have $" + cashOnHand + ". How many cups would you like to purchase?");
+			qtyCups = scan.nextInt();
+			cashOnHand = cashOnHand - (qtyCups * costofCups);
+			//String temp = scan.nextLine();
+
+			
+			if (cashOnHand < 0) {
+				System.out.println("You have no more cash :(");
+				System.out.println("Thanks for playing!");
+				boolean output = false
+			}
+			
+			else {
+				System.out.println("Your inventory: " + qtyLemons + " lemons, " + qtySugars + " cups of sugar, " + qtyIce + " cups of ice, and " + qtyCups + " cups");
+				System.out.println("You have $" + cashOnHand +  " left.");
+				boolean output = true
+			}
+			
+			return output
+		}                          
+		
 
 }        
 	
